0b93d978a5ca651b2eefb137046d699f60404541
GiKode <827266641@qq.com>
2021-03-03 13:47:08 +0800

    1. update
    Signed-off-by: GiKode <GiKode827266641@qq.com>

---
+++ rust/bindings.h
@@ -0,0 +1,10 @@
+/**
+ *
+ * network module for `lgj.wlwsx.client.flutter`
+ *
+ */
+char *rust_greeting(const char *to);
+
+void rust_cstr_free(char *s);
+
+char *net_test(void);
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,7 +6,6 @@ use std::ffi::{CString, CStr};
 ///
 
 /* example */
-
 #[no_mangle]
 pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
     let c_str = unsafe { CStr::from_ptr(to) };
@@ -28,11 +27,30 @@ pub extern fn rust_cstr_free(s: *mut c_char) {
 
 /* network */
 
-// #[no_mangle]
-// pub async fn net_test() -> Result<(), Box<dyn std::error::Error>> {
-//     // let o = minreq::get("https://speedtest.lax.hivelocity.net").send()?;
-//     // let s = o.as_str()?;
-//     // print!("{}", s);
-//     // Ok(())
-//     Ok(())
-// }
+#[no_mangle]
+pub async extern fn net_test() -> *mut c_char {
+    let params = [("foo", "bar"), ("baz", "quux")];
+    let client = reqwest::Client::new();
+    let response = client.post("http://httpbin.org/post")
+        .form(&params)
+        .send().await;
+
+    return match response {
+        Ok(res) => {
+            let pre_text = res.text().await;
+
+            let t2 = match pre_text {
+                Ok(data) => {
+                    CString::new(data).unwrap().into_raw()
+                }
+                Err(e2) => {
+                    CString::new(e2.to_string()).unwrap().into_raw()
+                }
+            };
+            t2
+        }
+        Err(err) => {
+            CString::new(err.to_string()).unwrap().into_raw()
+        }
+    };
+}
